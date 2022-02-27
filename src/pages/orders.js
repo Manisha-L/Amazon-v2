9d61c9a3d430ba289520e4be674915dc41ece8aa
Manisha-L <manisha.lakshmipathula3@gmail.com>
2022-02-27 16:55:56 +0000

    in progress bug fix

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { useSession, getSession } from "next-auth/react";
 import moment from "moment";
 import db from "../../firebase";
 import Order from "../../src/components/Order";
 
-function orders({ orders }) {
+function orders(props) {
   // const session = useSession();
   const { data: session } = useSession();
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    (async () => {
+      // const context = props.context;
+      const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+      // const session = await getSession(context);
+      const session = props.session;
+      console.log("session", session, props);
+      if (!props.user.email) {
+        return {
+          props: { orders: [] },
+        };
+      }
+      const stripeOrders = await db
+        .collection("users")
+        .doc(props.user.email)
+        .collection("orders")
+        .orderBy("timestamp", "desc")
+        .get();
+      console.log("Manisha", stripeOrders.docs);
+
+      const tst = stripeOrders.docs.map((order) => ({
+        id: order.id,
+        amount: order.data().amount,
+        amount_shipping: order.data().amount_shipping,
+        images: order.data().images,
+        timestamp: moment(order.data().timestamp.toDate()).unix(),
+        // items: (
+        //   await stripe.checkout.sessions.listLineItems(order.id, {
+        //     limit: 100,
+        //   })
+        // ).data,
+      }));
+      console.log("tst", tst);
+      // )
+      //   .then((res) => {
+      //     console.log("orderlocal", res);
+      //   })
+      //   .catch((e) => {
+      //     console.log("ordersError", e);
+      //   });
+
+      setOrders(tst);
+    })();
+  }, []);
 
   console.log(orders);
   return (
@@ -43,35 +88,41 @@ export default orders;
 
 export async function getServerSideProps(context) {
   const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
   const session = await getSession(context);
-  if (!session) {
-    return {
-      props: { orders: [] },
-    };
-  }
-  const stripeOrders = await db
-    .collection("users")
-    .doc(session.user.email)
-    .collection("orders")
-    .orderBy("timestamp", "desc")
-    .get();
+  console.log("check-session", session);
+  // const session = await getSession(context);
+  // if (!session) {
+  //   return {
+  //     props: { orders: [] },
+  //   };
+  // }
+  // const stripeOrders = await db
+  //   .collection("users")
+  //   .doc(session.user.email)
+  //   .collection("orders")
+  //   .orderBy("timestamp", "desc")
+  //   .get();
+  // console.log("Manisha", stripeOrders);
 
-  const orders = await Promise.all(
-    stripeOrders.docs.map(async (order) => ({
-      id: order.id,
-      amount: order.data().amount,
-      amount_shipping: order.data().amount_shipping,
-      images: order.data().images,
-      timestamp: moment(order.data().timestamp.toDate()).unix(),
-      items: (
-        await stripe.checkout.sessions.listLineItems(order.id, {
-          limit: 100,
-        })
-      ).data,
-    }))
-  );
+  // const orders = await Promise.all(
+  //   stripeOrders.docs.map(async (order) => ({
+  //     id: order.id,
+  //     amount: order.data().amount,
+  //     amount_shipping: order.data().amount_shipping,
+  //     images: order.data().images,
+  //     timestamp: moment(order.data().timestamp.toDate()).unix(),
+  //     items: (
+  //       await stripe.checkout.sessions.listLineItems(order.id, {
+  //         limit: 100,
+  //       })
+  //     ).data,
+  //   }))
+  // );
   return {
-    props: { orders },
+    // props: { orders },
+    props: {
+      session,
+      user: session.user,
+    },
   };
 }
